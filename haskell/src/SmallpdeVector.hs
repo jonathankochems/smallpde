e8f1a63b83982d73a4ce0901172f2e83f99ee58f
Jonathan Kochems <jonathan.kochems@gmail.com>
2016-08-02 01:11:12 +0200

    adding 16 iteration

--- haskell/src/SmallpdeVector.hs
+++ haskell/src/SmallpdeVector.hs
@@ -15,10 +15,21 @@ import Numeric (showEFloat)
 
 import VectorInst
 
-{-# INLINE eightIter' #-} 
-eightIter' !n !d !a !b !i !j = $(stencilBody 8)
+{-# INLINE fourIter #-} 
+fourIter !n !d !a !b !i !j = $(stencilBody 4)
     where {-# INLINE pureStencil #-} 
           pureStencil = pureStencil' d 
+
+{-# INLINE eightIter #-} 
+eightIter !n !d !a !b !i !j = $(stencilBody 8)
+    where {-# INLINE pureStencil #-} 
+          pureStencil = pureStencil' d 
+
+{-# INLINE sixtIter #-} 
+sixtIter !n !d !a !b !i !j = $(stencilBody 16)
+    where {-# INLINE pureStencil #-} 
+          pureStencil = pureStencil' d 
+
 
 tester :: IO ()
 tester = do b <- VU.thaw $ VU.fromList [if x == 12 then 1 else 0 | x <- [0..25]]
@@ -82,28 +93,56 @@ solve !n !iterations =
   where {-# INLINE onePass #-}
         onePass !n !d !a !b = go 1 1
            where go !i !j | i == n-1 = return ()
-                          | j >= n-8 && j == n-1 = go (i+1) 1
-                          | j >= n-8 && j == n-2 = do !() <- oneIter n d a b i j
-                                                      go (i+1) 1
-                          | j >= n-8 && j == n-3 = do !() <- twoIter n d a b i j
-                                                      go (i+1) 1
-                          | j >= n-8 && j == n-4 = do !() <- twoIter n d a b i j
-                                                      !() <- oneIter n d a b i (j+2)
-                                                      go (i+1) 1 
-                          | j >= n-8 && j == n-5 = do !() <- fourIter n d a b i j
-                                                      go (i+1) 1
-                          | j >= n-8 && j == n-6 = do !() <- fourIter n d a b i j
-                                                      !() <- oneIter n d a b i (j+4)
-                                                      go (i+1) 1
-                          | j >= n-8 && j == n-7 = do !() <- fourIter n d a b i j
-                                                      !() <- twoIter n d a b i (j+4)
-                                                      go (i+1) 1
-                          | j >= n-8 && j == n-8 = do !() <- fourIter n d a b i j
-                                                      !() <- twoIter n d a b i (j+4)
-                                                      !() <- oneIter n d a b i (j+6)
-                                                      go (i+1) 1
-                          | otherwise            = do !() <- eightIter' n d a b i j
-                                                      go i (j+8)
+                          | j >= n-16 && j == n-1 = go (i+1) 1
+                          | j >= n-16 && j == n-2 = do !() <- oneIter n d a b i j
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-3 = do !() <- twoIter n d a b i j
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-4 = do !() <- twoIter n d a b i j
+                                                       !() <- oneIter n d a b i (j+2)
+                                                       go (i+1) 1 
+                          | j >= n-16 && j == n-5 = do !() <- fourIter n d a b i j
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-6 = do !() <- fourIter n d a b i j
+                                                       !() <- oneIter n d a b i (j+4)
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-7 = do !() <- fourIter n d a b i j
+                                                       !() <- twoIter n d a b i (j+4)
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-8 = do !() <- fourIter n d a b i j
+                                                       !() <- twoIter n d a b i (j+4)
+                                                       !() <- oneIter n d a b i (j+6)
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-9 = do !() <- eightIter n d a b i j
+                                                       go (i+1) 1
+                          | j >= n-16 && j == n-10 = do !() <- eightIter n d a b i j
+                                                        !() <- oneIter n d a b i (j+8)
+                                                        go (i+1) 1
+                          | j >= n-16 && j == n-11 = do !() <- eightIter n d a b i j
+                                                        !() <- twoIter n d a b i (j+8)
+                                                        go (i+1) 1
+                          | j >= n-16 && j == n-12 = do !() <- eightIter n d a b i j
+                                                        !() <- twoIter n d a b i (j+8)
+                                                        !() <- oneIter n d a b i (j+10)
+                                                        go (i+1) 1 
+                          | j >= n-16 && j == n-13 = do !() <- eightIter n d a b i j
+                                                        !() <- fourIter n d a b i (j+8)
+                                                        go (i+1) 1
+                          | j >= n-16 && j == n-14 = do !() <- eightIter n d a b i j
+                                                        !() <- fourIter n d a b i (j+8)
+                                                        !() <- oneIter n d a b i (j+12)
+                                                        go (i+1) 1
+                          | j >= n-16 && j == n-15 = do !() <- eightIter n d a b i j
+                                                        !() <- fourIter n d a b i (j+8)
+                                                        !() <- twoIter n d a b i (j+12)
+                                                        go (i+1) 1
+                          | j >= n-16 && j == n-16 = do !() <- eightIter n d a b i j
+                                                        !() <- fourIter n d a b i (j+8)
+                                                        !() <- twoIter n d a b i (j+12)
+                                                        !() <- oneIter n d a b i (j+14)
+                                                        go (i+1) 1
+                          | otherwise            = do !() <- sixtIter n d a b i j
+                                                      go i (j+16)
                           {-| otherwise            = do !() <- fourIter n d a b i j
                                                       go i (j+4)-}
         {-# INLINE oneIter #-} 
@@ -119,7 +158,6 @@ solve !n !iterations =
                                                                 + west
                                                                 + south
                                                                )
-                  --traceShow ("one",(1-4*d) * here + d*( north + east + west + south)) $ return ()
                   return()
         {-# INLINE twoIter #-} 
         twoIter !n !d !a !b !i !j = do 
@@ -147,132 +185,6 @@ solve !n !iterations =
                                                                    + west'
                                                                    + south'
                                                                  )
-                  --traceShow ("two",(1-4*d) * here + d*( north + east + west + south),
-                  --                 (1-4*d) * here'+ d*( north' + east' + west' + south')) $ return ()
-                  return ()
-        {-# INLINE fourIter #-} 
-        fourIter !n !d !a !b !i !j = do 
-                  !north   <- VGM.unsafeRead b (n*(i+1)+j)
-                  !north'  <- VGM.unsafeRead b (n*(i+1)+j+1)
-                  !north'' <- VGM.unsafeRead b (n*(i+1)+j+2)                  
-                  !north'''<- VGM.unsafeRead b (n*(i+1)+j+3)                  
-                  !east    <- VGM.unsafeRead b (n*i+j-1)
-                  !here    <- VGM.unsafeRead b (n*i+j)
-                  !west    <- VGM.unsafeRead b (n*i+j+1)
-                  !west'   <- VGM.unsafeRead b (n*i+j+2)
-                  !west''  <- VGM.unsafeRead b (n*i+j+3)
-                  !west''' <- VGM.unsafeRead b (n*i+j+4)
-                  !south   <- VGM.unsafeRead b (n*(i-1)+j)
-                  !south'  <- VGM.unsafeRead b (n*(i-1)+j+1)
-                  !south'' <- VGM.unsafeRead b (n*(i-1)+j+2)
-                  !south'''<- VGM.unsafeRead b (n*(i-1)+j+3)
-                  let !east'  = here
-                      !east'' = west
-                      !east'''= west'
-                      !here'  = west
-                      !here'' = west'
-                      !here'''= west''
-                      !new    = (1-4*d) * here + d*(   north + east
-                                                     + west  + south
-                                                   )
-                      !new'   = (1-4*d) * here' + d*(  north' + east'
-                                                     + west'  + south'
-                                                   )
-                      !new''  = (1-4*d) * here''+ d*(  north''+ east''
-                                                     + west'' + south''
-                                                   )
-                      !new''' = (1-4*d) * here'''+ d*( north'''+ east'''
-                                                       + west''' + south'''
-                                                     )
-                  --traceShow ("four",new, new', new'', new''') $ return ()
-                  !()    <- VGM.unsafeWrite a (n*i+j)   $! new
-                  !()    <- VGM.unsafeWrite a (n*i+j+1) $! new'
-                  !()    <- VGM.unsafeWrite a (n*i+j+2) $! new''
-                  !()    <- VGM.unsafeWrite a (n*i+j+3) $! new'''
-                  return ()
-        {-# INLINE eightIter #- } 
-        eightIter !n !d !a !b !i !j = do 
-                  !north   <- VGM.unsafeRead b (n*(i+1)+j)
-                  !north'  <- VGM.unsafeRead b (n*(i+1)+j+1)
-                  !north'' <- VGM.unsafeRead b (n*(i+1)+j+2)                  
-                  !north'''<- VGM.unsafeRead b (n*(i+1)+j+3)                  
-                  !northiv <- VGM.unsafeRead b (n*(i+1)+j+4)
-                  !northv  <- VGM.unsafeRead b (n*(i+1)+j+5)
-                  !northv' <- VGM.unsafeRead b (n*(i+1)+j+6)                  
-                  !northv''<- VGM.unsafeRead b (n*(i+1)+j+7)                  
-                  !east    <- VGM.unsafeRead b (n*i+j-1)
-                  !here    <- VGM.unsafeRead b (n*i+j)
-                  !west    <- VGM.unsafeRead b (n*i+j+1)
-                  !west'   <- VGM.unsafeRead b (n*i+j+2)
-                  !west''  <- VGM.unsafeRead b (n*i+j+3)
-                  !west''' <- VGM.unsafeRead b (n*i+j+4)
-                  !westiv  <- VGM.unsafeRead b (n*i+j+5)
-                  !westv   <- VGM.unsafeRead b (n*i+j+6)
-                  !westv'  <- VGM.unsafeRead b (n*i+j+7)
-                  !westv'' <- VGM.unsafeRead b (n*i+j+8)                  
-                  !south   <- VGM.unsafeRead b (n*(i-1)+j)
-                  !south'  <- VGM.unsafeRead b (n*(i-1)+j+1)
-                  !south'' <- VGM.unsafeRead b (n*(i-1)+j+2)
-                  !south'''<- VGM.unsafeRead b (n*(i-1)+j+3)
-                  !southiv <- VGM.unsafeRead b (n*(i-1)+j+4)
-                  !southv  <- VGM.unsafeRead b (n*(i-1)+j+5)
-                  !southv' <- VGM.unsafeRead b (n*(i-1)+j+6)
-                  !southv''<- VGM.unsafeRead b (n*(i-1)+j+7)
-                  let !east'   = here
-                      !east''  = west
-                      !east''' = west'
-                      !eastiv  = west''
-                      !eastv   = west'''
-                      !eastv'  = westiv
-                      !eastv'' = westv
-                      !here'   = west
-                      !here''  = west'
-                      !here''' = west''
-                      !hereiv  = west'''
-                      !herev   = westiv
-                      !herev'  = westv
-                      !herev'' = westv'
-                      new0 :: Float
-                      !new0   = (1-4*d) * here + d*(   north + east
-                                                     + west  + south
-                                                   )
-                      new' :: Float
-                      !new'   = (1-4*d) * here' + d*(  north' + east'
-                                                     + west'  + south'
-                                                   )
-                      new'' :: Float
-                      !new''  = (1-4*d) * here''+ d*(  north''+ east''
-                                                     + west'' + south''
-                                                   )
-                      new''' :: Float
-                      !new''' = (1-4*d) * here'''+ d*( north'''+ east'''
-                                                       + west''' + south'''
-                                                     )
-                      newiv :: Float
-                      !newiv  = (1-4*d) * hereiv + d*( northiv + eastiv
-                                                      + westiv  + southiv
-                                                     )
-                      newv :: Float
-                      !newv   = (1-4*d) * herev + d*(  northv + eastv
-                                                     + westv  + southv
-                                                   )
-                      newv' :: Float
-                      !newv'  = (1-4*d) * herev'+ d*(  northv'+ eastv' 
-                                                     + westv' + southv'
-                                                   )
-                      newv'' :: Float
-                      !newv'' = (1-4*d) * herev''+ d*( northv''+ eastv''
-                                                       + westv'' + southv''
-                                                     )
-                  --traceShow ("eight",new0, new', new'', new''', newiv, newv, newv', newv'') $ return ()
-                  !()    <- VGM.unsafeWrite a (n*i+j)   $! new0
-                  !()    <- VGM.unsafeWrite a (n*i+j+1) $! new'
-                  !()    <- VGM.unsafeWrite a (n*i+j+2) $! new''
-                  !()    <- VGM.unsafeWrite a (n*i+j+3) $! new'''
-                  !()    <- VGM.unsafeWrite a (n*i+j+4) $! newiv
-                  !()    <- VGM.unsafeWrite a (n*i+j+5) $! newv
-                  !()    <- VGM.unsafeWrite a (n*i+j+6) $! newv'
-                  !()    <- VGM.unsafeWrite a (n*i+j+7) $! newv''
                   return ()
 
 main = do let n :: Int
