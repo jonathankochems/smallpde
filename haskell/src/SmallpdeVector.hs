8d76878439a9990781cbe979d4a7af8b006fe32c
Jonathan Kochems <jonathan.kochems@gmail.com>
2016-09-24 22:47:00 +0100

    explicit loops for time and IO

--- haskell/src/SmallpdeVector.hs
+++ haskell/src/SmallpdeVector.hs
@@ -9,7 +9,7 @@ import qualified Data.List as List
 import qualified Data.Vector.Generic.Mutable as VGM
 import qualified Data.Vector.Unboxed as VU
 import Data.Vector.Generic.Mutable(new, read, write, unsafeRead, unsafeWrite, set)
--- import Control.Monad (forM{-, forM_-})
+import Control.Monad (forM, forM_)
 import Control.Monad.Primitive (PrimState)
 import Debug.Trace(traceShow)
 import Numeric (showEFloat)
@@ -39,49 +39,6 @@ unsafePurePerfMarker x = (unsafePerformIO perf_marker) `seq` x
 
 when True  m = m
 when False _ = return ()
-
-
---foldr            :: (a -> b -> b) -> b -> [a] -> b
--- foldr _ z []     =  z
--- foldr f z (x:xs) =  f x (foldr f z xs)
-{-# INLINE [0] foldr #-}
--- Inline only in the final stage, after the foldr/cons rule has had a chance
--- Also note that we inline it when it has *two* parameters, which are the
--- ones we are keen about specialising!
-foldr k z = go
-          where
-            --go :: [a] -> b
-            go []     = z
-            go (y:ys) = unsafePurePerfMarker ( y `k` go ys )
-
-
-mapM_ :: (a -> IO b) -> [a] -> IO ()
-mapM_ f= foldr ((>>) . f') (return ())
-  where {-# INLINE f' #-}
-        f' x = do perf_marker
-                  f x
--- | 'forM_' is 'mapM_' with its arguments flipped. For a version that
--- doesn't ignore the results see 'Data.Traversable.forM'.
---
--- As of base 4.8.0.0, 'forM_' is just 'for_', specialized to 'Monad'.
-forM_ :: [a] -> (a -> IO b) -> IO ()
-{-# INLINE forM_ #-}
-forM_ = flip mapM_
-
--- | 'forM' is 'mapM' with its arguments flipped. For a version that
--- ignores the results see 'Data.Foldable.forM_'.
-forM ::  [a] -> (a -> IO b) -> IO [b]
-{-# INLINE forM #-}
-forM = flip mapM
-
-{-# INLINE mapM #-} -- so that traverse can fuse
-mapM ::  (a -> IO b) -> [a] -> IO [b]
-mapM f = foldr cons_f (pure [])
-  where {-# INLINE cons_f #-}
-        cons_f x ys = (:) <$> f' x <*> ys
-        {-# INLINE f' #-}
-        f' x = do perf_marker
-                  f x
 
 {-# INLINE pureStencil' #-}
 --pureStencil' :: Float -> Float -> Float -> Float -> Float -> Float -> Float
@@ -160,14 +117,15 @@ solve !n !iterations =
            !a <- VU.unsafeThaw a'
            !b <- VU.unsafeThaw b'
            !() <- VGM.unsafeWrite a (indexTransform n halfn halfn) 1.0 
-           !() <- forM_ [0..steps-1] (\_ -> do
-                    perf_marker
-                    !() <- onePass n d d' b a
-                    -- printArray n b
-                    !() <- onePass n d d' a b
-                    -- printArray n a
-                    return ()
-                 )
+           !() <- timeloop steps n d d' a b
+           -- !() <- forM_ [0..steps-1] (\_ -> do
+           --          --perf_marker
+           --          !() <- onePass n d d' b a
+           --          -- printArray n b
+           --          !() <- onePass n d d' a b
+           --          -- printArray n a
+           --          return ()
+           --       )
            !() <-  when (iterations `mod` 2 == 1) $ do
                     !() <- onePass n d d' b a
                     -- printArray n b
@@ -176,7 +134,17 @@ solve !n !iterations =
            -- printArray n a
            -- printArrayRaw n a
            return a
-  where {-# INLINE onePass #-}
+  where {-# INLINE timeloop #-}
+        timeloop :: Int -> Int -> FloatX4 -> Float -> VU.MVector (PrimState IO) Float -> VU.MVector (PrimState IO) Float -> IO ()
+        timeloop !steps !n !d !d' !a !b = go 0 
+          where go !i | i < steps = do --perf_marker
+                                       !() <- onePass n d d' b a
+                                       -- printArray n b
+                                       !() <- onePass n d d' a b
+                                       -- printArray n a
+                                       go (i+1)
+                      | otherwise = return ()
+        {-# INLINE onePass #-}
         onePass :: Int -> FloatX4 -> Float -> VU.MVector (PrimState IO) Float -> VU.MVector (PrimState IO) Float -> IO ()
         onePass !n !d !d' !a !b = go'' 0 1
            where go !i !j | i+4*n >= n*n-2 = -- traceShow (1,i,j,indexTransform n i j) $ 
@@ -192,7 +160,7 @@ solve !n !iterations =
                                             go' (i+1) 1
                            | otherwise = do -- traceShow (6,i,j,indexTransform n i j) $ return ()
                                             !() <- oneIter' n d a b i j
-                                            perf_marker
+                                            --perf_marker
                                             go' i (j+1)
                  go'' !i !j | i > 0 = -- traceShow (7,i,j,indexTransform n i j) $
                                            go (4*n) 4
@@ -200,12 +168,12 @@ solve !n !iterations =
                                             go'' (i+1) 1
                             | otherwise = do -- traceShow (9,i,j,indexTransform n i j) $ return ()
                                              !() <- oneIter'' n d a b i j
-                                             perf_marker
+                                             --perf_marker
                                              go'' i (j+1)
 
         {-# INLINE oneIter #-}
         oneIter !n !d !a !b !i !j = do
-                  perf_marker
+                  --perf_marker
                   !north  <-  VUS.veryunsafeVectorisedRead b  $ i+4*n+j
                   !east   <- VUS.veryunsafeVectorisedRead b  $ i+j-4
                   !here   <- VUS.veryunsafeVectorisedRead b  $ i+j
@@ -217,7 +185,7 @@ solve !n !iterations =
                                                                 + west
                                                                 + south
                                                                )
-                  perf_marker
+                  --perf_marker
                   !north' <- VUS.veryunsafeVectorisedRead b  $ i+4*n+j+4
                   !west'  <- VUS.veryunsafeVectorisedRead b  $ i+j+8
                   !south' <- VUS.veryunsafeVectorisedRead b  $ i-4*n+j+4
@@ -245,7 +213,7 @@ solve !n !iterations =
                                            )
                 !new'= unsafeInsertVector new 0 3
             !()    <- VUS.veryunsafeVectorisedWrite a  (4*n*i+4*j) $! new' 
-	    perf_marker
+            --perf_marker
             return()
 
         {-# INLINE oneIter'' #-} 
@@ -265,30 +233,25 @@ solve !n !iterations =
                                            )
                 !new'= unsafeInsertVector new 0 0
             !()    <- VUS.veryunsafeVectorisedWrite a  (4*n*i+4*j) $! new' 
-	    perf_marker
+            --perf_marker
             return()
 
 
-printArray n a = do forM_ [0..3] $! \k -> do
-                        perf_marker
-                        !() <- forM_ [0..n `div` 4 -1] (\(!i) -> do
-                                 perf_marker
-                                 --when (i `mod` slice == 0) $ putStrLn ""
-                                 !liness <- forM [0..n-1] (\(!j) -> do
-                                              !num <- VGM.unsafeRead a $ 4*n*i+4*j+k
-                                              perf_marker
-                                              return $! showEFloat (Just 5) num " ")
-                                 putStrLn $! concat liness)
-                        return ()
-                    putStrLn ""
-                    putStrLn ""
+printArray n a = go 0 0 (n-1) "" 
     where !slice = (n+3) `div` 4
+          go !k !i !j !line | k >= 4 = do putStrLn ""
+                                          putStrLn ""
+                            | i >= n `div` 4 = go (k+1) 0 (n-1) ""
+                            | j >= 0 = do !num <- VGM.unsafeRead a $ 4*n*i+4*j+k
+                                          go k i (j-1) $! (showEFloat (Just 5) num " "++line)
+                            | j < 0  = do putStrLn line
+                                          go k (i+1) (n-1) ""
     
 printArrayRaw n a = do forM_ [0..n-1] (\(!i) -> do
                          --when (i `mod` slice == 0) $ putStrLn ""
                          !liness <- forM [0..n-1] (\(!j) -> do
                                       !num <- VGM.unsafeRead a $ n*i+j 
-                                      perf_marker
+                                      --perf_marker
                                       return $! showEFloat (Just 5) num " ")
                          putStrLn $! concat liness)
                        putStrLn ""
